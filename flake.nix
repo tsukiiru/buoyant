57c001ef76edbcab95898b3f3f9fdb2469b4f3e0
tsuki <108170925+tsukiiru@users.noreply.github.com>
2026-08-11 13:44:02 +0700

    remove fhs from flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,54 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
+      rust-overlay,
+      flake-utils,
       ...
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          let
+            essentials = [
+              pkg-config
+              clang
+              clippy
+              rust-bin.beta.latest.default
+            ];
+          in
+          mkShell rec {
+            buildInputs = essentials ++ [
+              mold
+              stdenv.cc.cc.lib
+              libX11
+              libXcursor
+              libXrandr
+              libXi
+              libxcb
+              libxkbcommon
+              vulkan-loader
+              wayland
+            ];
 
-      fhs = pkgs.buildFHSEnv {
-        name = "rust-build-env";
-        targetPkgs =
-          pkgs: with pkgs; [
-            pkg-config
-            libxkbcommon
-            libxkbcommon.dev
-          ];
-        multiPkgs =
-          pkgs: with pkgs; [
-            clang
-            zlib
-            mold
-            stdenv.cc.cc.lib
-            libX11
-            libXcursor
-            libXrandr
-            libXi
-            libxcb
-            vulkan-loader
-            wayland
-          ];
-        runScript = "fish";
-      };
-    in
-    {
-      devShells.${system}.default = fhs.env;
-    };
+            shellHook = ''
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+            '';
+          };
+      }
+    );
 }
