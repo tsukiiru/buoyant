32c0973576caba61fa5f667e22b377c493218ac9
tsukiiru <108170925+tsukiiru@users.noreply.github.com>
2026-09-01 19:33:10 +0700

    change app id

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::sync::Arc;
+
+use eframe::egui::{IconData, ViewportBuilder};
+
 mod app;
 mod config;
 mod file;
@@ -5,7 +9,15 @@ mod ui;
 
 #[tokio::main]
 async fn main() -> eframe::Result {
-    let native_opts = eframe::NativeOptions::default();
+    let native_opts = eframe::NativeOptions {
+        viewport: ViewportBuilder {
+            title: Some("buoyant".into()),
+            app_id: Some("buoyant".into()),
+            icon: Some(Arc::new(IconData::default())),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
 
     eframe::run_native(
         "buoyant",
