230a29182deb23e729dcc822d8584617d1b4f531
tsukiiru <108170925+tsukiiru@users.noreply.github.com>
2026-09-02 14:26:00 +0700

    hmm

--- flake.nix
+++ flake.nix
@@ -1,62 +1,112 @@
 {
+  description = "a file explorer for linux";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      fenix,
-      ...
-    }:
+    { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      packages.${system}.default = fenix.packages.${system}.minimal.toolchain;
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ fenix.overlays.default ];
-      };
-      rustToolchain = pkgs.fenix.complete.withComponents [
-        "cargo"
-        "clippy"
-        "rustfmt"
-        "rustc"
-        "rust-src"
-      ];
+      revision = self.shortRev or self.dirtyShortRev or "unknown";
+      buoyant-package =
+        {
+          lib,
+          pkg-config,
+          rustPlatform,
+        }:
+        rustPlatform.buildRustPackage {
+          pname = "buoyant";
+          version = revision;
+
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./assets
+              ./src
+              ./Cargo.toml
+              ./Cargo.lock
+            ];
+          };
+
+          cargoLock = {
+            allowBuiltinFetchGit = true;
+            lockFile = ./Cargo.lock;
+          };
+
+          strictDeps = false;
+
+          nativeBuildInputs = [
+            rustPlatform.bindgenHook
+            pkg-config
+          ];
+        };
+
+      inherit (nixpkgs) lib;
+      systems = lib.intersectLists lib.systems.flakeExposed lib.platforms.linux;
+
+      forAllSystems = lib.genAttrs systems;
+      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
-      devShells.${system}.default =
-        with pkgs;
+      devShells = forAllSystems (
+        system:
         let
-          essentials = [
-            pkg-config
-            clang
-            rustToolchain
-            rust-analyzer-nightly
-          ];
+          pkgs = nixpkgsFor.${system};
+          rustfmt' = pkgs.rustfmt.override { asNightly = true; };
+          inherit (self.packages.${system}) buoyant;
         in
-        mkShell rec {
-          buildInputs = essentials ++ [
-            mold
-            stdenv.cc.cc.lib
-            libX11
-            libXcursor
-            libXrandr
-            libXi
-            libxcb
-            libxkbcommon
-            vulkan-loader
-            wayland
-          ];
+        {
+          default = pkgs.mkShell {
+            packages = builtins.attrValues {
+              inherit (pkgs)
+                rustc
+                cargo
+                clippy
+                rust-analyzer
+                ;
+              inherit rustfmt';
+            };
 
-          shellHook = ''
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
-          '';
-        };
+            nativeBuildInputs = with pkgs; [
+              rustPlatform.bindgenHook
+              pkg-config
+              mold
+              stdenv.cc.cc.lib
+              libX11
+              libXcursor
+              libXrandr
+              libXi
+              libxcb
+              libxkbcommon
+              vulkan-loader
+              wayland
+            ];
+
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+            buildInputs = buoyant.buildInputs;
+            shellHook = ''
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buoyant.buildInputs)}";
+            '';
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt);
+
+      packages = forAllSystems (
+        system:
+        let
+          buoyant = nixpkgsFor.${system}.callPackage buoyant-package { };
+        in
+        {
+          inherit buoyant;
+          default = buoyant;
+        }
+      );
+
+      overlays.default = final: _: {
+        buoyant = final.callPackage buoyant-package { };
+      };
     };
 }
